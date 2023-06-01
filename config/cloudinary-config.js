adcfa4528639b3ed5e0ab51aba6a28ea0cbe075a
Ibrokhim3 <ishamasiddinov.97@gmail.com>
2023-06-01 13:25:10 +0500

    connected to the MongoDb real

--- config/cloudinary-config.js
+++ config/cloudinary-config.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dephdgqpo",
-  api_key: "226979763886815",
-  api_secret: "UtSxAxFzkpjUHxKQAiXS3fANfyo",
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 
--- config/connect-to-db.js
+++ config/connect-to-db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     mongoose
-      .connect("mongodb://localhost:27017/pressa", {
+      .connect(process.env.MONGO_PORT, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
